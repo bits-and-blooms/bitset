c344bb4d059f128e61e2d2b80019dac724a29c57
Will Fitzgerald <will.fitzgerald@gmail.com>
2011-05-11 08:08:22 -0400

    more defensive BitSets; carry maximum size, which is queryable

--- bitset.go
+++ bitset.go
@@ -1,21 +1,17 @@
-// Copyright 2011 Will Fitzgerald. All rights reserved.
+// Copyright 2011 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 /*
 	Package bitset implements bitsets.
-	
+
 	It provides methods for making a BitSet of an arbitrary
 	upper limit, setting and testing bit locations, and clearing
 	bit locations as well as the entire set.
-	
-	BitSets are implemented as arrays of uint64s, so it may be best
-	to limit the upper size to a multiple of 64. It is not an error
-	to set, test, or clear a bit within a 64 bit boundary.
-	
+
 	Example use:
-	
-	b := MakeBitSet(64000)
+
+	b := bitset.New(64000)
 	b.SetBit(1000)
 	if b.Bit(1000) {
 		b.ClearBit(1000)
@@ -23,35 +19,49 @@
 */
 package bitset
 
+// BitSet internal details 
+type BitSet struct {
+	max_size uint
+	set      []uint64
+}
 
-// BitSets are arrays of uint64. 
-type BitSet []uint64
+// Make a BitSet with an upper limit on size.
+func New(max_size uint) *BitSet {
+	return &BitSet{max_size, make([]uint64, (max_size+(64-1))/64)}
+}
 
-// Make a BitSet with an upper limit on size. Note this is the
-// number of bits, not the number of uint64s, which is a kind of
-// implementation detail.
-func MakeBitSet(max_size uint) BitSet {
-	return make(BitSet, (max_size+(64-1))/64)
+// Query maximum size of a bit set
+func (b *BitSet) MaxSize() uint {
+	return b.max_size
 }
 
 /// Test whether bit i is set. 
-func (set BitSet) Bit(i uint) bool {  
-	return ((set[i/64] & (1<<(i%64)))!=0)
+func (b *BitSet) Bit(i uint) bool {
+	if b != nil && i < b.max_size {
+		return ((b.set[i/64] & (1 << (i % 64))) != 0)
+	}
+	return false
 }
 
 // Set bit i to 1
-func (set BitSet) SetBit(i uint) {
-	set[i/64]|=(1<<(i%64))
+func (b *BitSet) SetBit(i uint) {
+	if b != nil && i < b.max_size {
+		b.set[i/64] |= (1 << (i % 64))
+	}
 }
 
 // Clear bit i to 0
-func (set BitSet) ClearBit(i uint) {
-	set[i/64] &^= 1 << (i % 64)
+func (b *BitSet) ClearBit(i uint) {
+	if b != nil && i < b.max_size {
+		b.set[i/64] &^= 1 << (i % 64)
+	}
 }
 
 // Clear entire BitSet
-func (set BitSet) Clear() {
-	for i,_ := range set {
-		set[i] = 0
+func (b *BitSet) Clear() {
+	if b != nil {
+		for i, _ := range b.set {
+			b.set[i] = 0
+		}
 	}
 }
--- bitset_test.go
+++ bitset_test.go
@@ -9,15 +9,15 @@ import (
 	"testing"
 )
 
-func TestMakeBitSet(t *testing.T) {
-	v := MakeBitSet(10000)
+func TestbBitSetNew(t *testing.T) {
+	v := New(10000)
 	if v.Bit(0) != false {
 		t.Errorf("Unable to make a bit set and read its 0th value.")
 	}
 }
 
 func TestBitSetIsClear(t *testing.T) {
-	v := MakeBitSet(1000)
+	v := New(1000)
 	for i := uint(0); i < 1000; i++ {
 		if v.Bit(i) != false {
 			t.Errorf("Bit %d is set, and it shouldn't be.", i)
@@ -27,7 +27,7 @@ func TestBitSetIsClear(t *testing.T) {
 
 
 func TestBitSetAndGet(t *testing.T) {
-	v := MakeBitSet(1000)
+	v := New(1000)
 	v.SetBit(100)
 	if v.Bit(100) != true {
 		t.Errorf("Bit %d is clear, and it shouldn't be.", 100)
@@ -36,7 +36,7 @@ func TestBitSetAndGet(t *testing.T) {
 
 func TestLotsOfSetsAndGets(t *testing.T) {
 	tot := uint(100000)
-	v := MakeBitSet(tot)
+	v := New(tot)
 	for i := uint(0); i < tot; i+=2 {
 		v.SetBit(i)
 	}
@@ -55,7 +55,7 @@ func TestLotsOfSetsAndGets(t *testing.T) {
 
 func TestClear(t *testing.T) {
 	tot := uint(1000)
-	v := MakeBitSet(tot)
+	v := New(tot)
 	for i := uint(0); i < tot; i++ {
 		v.SetBit(i)
 	}
@@ -69,21 +69,28 @@ func TestClear(t *testing.T) {
 }
 
 func TestOutOfBoundsBad(t *testing.T) {
-	v := MakeBitSet(64)
+	v := New(64)
 	defer func() {
-	        if r := recover(); r == nil {
-	            t.Error("Out of index error within the next set of bits should have caused a panic")
+	        if r := recover(); r != nil {
+	            t.Error("Out of index error within the next set of bits should not have caused a panic")
 	        }
 	    }()
 	v.SetBit(1000)
 }
 
 func TestOutOfBoundsOK(t *testing.T) {
-	v := MakeBitSet(65)
+	v := New(65)
 	defer func() {
 	        if r := recover(); r != nil {
 	            t.Error("Out of index error within the next set of bits should not caused a panic")
 	        }
 	    }()
 	v.SetBit(66)   
+}
+
+func TestMaxSizet(t *testing.T) {
+	v := New(1000)
+	if v.MaxSize() != 1000 {
+		t.Errorf("MaxSize should be 1000, but is %d.", v.MaxSize())
+	}
 }
