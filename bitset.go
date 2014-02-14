15635d3d060bcde331c4bdb6bb6fcca77d811a26
Daniel Lemire <lemire@gmail.com>
2014-02-14 01:24:09 -0500

    Undoing formatting changes

--- bitset.go
+++ bitset.go
@@ -270,12 +270,12 @@ const h01 uint64 = 0x0101010101010101 //the sum of 256 to the power of 0,1,2,3..
 // This is algorithm popcount_2 in the article retrieved May 9, 2011
 
 func popcount_2(x uint64) uint64 {
-	x -= (x >> 1) & m1	           //put count of each 2 bits into those 2 bits
+	x -= (x >> 1) & m1             //put count of each 2 bits into those 2 bits
 	x = (x & m2) + ((x >> 2) & m2) //put count of each 4 bits into those 4 bits
-	x = (x + (x >> 4)) & m4	       //put count of each 8 bits into those 8 bits
-	x += x >> 8		               //put count of each 16 bits into their lowest 8 bits
-	x += x >> 16		           //put count of each 32 bits into their lowest 8 bits
-	x += x >> 32		           //put count of each 64 bits into their lowest 8 bits
+	x = (x + (x >> 4)) & m4        //put count of each 8 bits into those 8 bits
+	x += x >> 8                    //put count of each 16 bits into their lowest 8 bits
+	x += x >> 16                   //put count of each 32 bits into their lowest 8 bits
+	x += x >> 32                   //put count of each 64 bits into their lowest 8 bits
 	return x & 0x7f
 }
  
